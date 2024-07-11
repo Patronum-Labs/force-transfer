a03e922f276983eae78fe6bd33e25eec4628f6ff
YamenMerhi <yamennmerhi@gmail.com>
2024-07-12 00:06:56 +0300

    refactor: modify config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,20 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10_000_000,
+      },
+      evmVersion: "paris", // Prevent using the `PUSH0` opcode
+      viaIR: false, 
+      metadata: {
+        bytecodeHash: "none", // Remove the metadata hash from the bytecode
+      },
+    },
+  },
 };
 
 export default config;
