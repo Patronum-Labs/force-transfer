5a3622f0644fa17802715fe4cb684c4e5aa6562b
YamenMerhi <yamennmerhi@gmail.com>
2024-08-03 13:20:54 +0300

    feat: add event to ForceTransfer

--- contracts/ForceTransfer.sol
+++ contracts/ForceTransfer.sol
@@ -9,6 +9,16 @@ import {SelfDestructContract} from "./SelfDestructContract.sol";
 /// @dev Utilizes a self-destructing contract to bypass recipient limitations
 /// @custom:security EIP-6049 deprecated selfdestruct as it may be subject to breaking changes in the future, use on your own risk
 contract ForceTransfer {
+    /// @notice Emitted when a forced transfer is executed
+    /// @param sender The address that initiated the force transfer
+    /// @param recipient The address that received the forced transfer
+    /// @param amount The amount of value that was transferred
+    event ForcedTransfer(
+        address indexed sender,
+        address indexed recipient,
+        uint256 amount
+    );
+
     /// @notice Forces value transfer to a specified recipient
     /// @dev Creates a new SelfDestructContract with the amount being sent and the specified recipient
     /// @param recipient The address to receive the value
@@ -16,5 +26,6 @@ contract ForceTransfer {
     /// is a contract without a receive function
     function force(address payable recipient) external payable {
         new SelfDestructContract{value: msg.value}(recipient);
+        emit ForcedTransfer(msg.sender, recipient, msg.value);
     }
 }
